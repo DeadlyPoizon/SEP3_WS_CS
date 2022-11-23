ddb62755393023d97901eecdc6d28c4e3f1e9a28
EKKA_GITHUB <48787232+DeadlyPoizon@users.noreply.github.com>
2022-11-23 13:43:24 +0100

    fwfw

--- GRPCService/Program.cs
+++ GRPCService/Program.cs
@@ -4,22 +4,37 @@ using Grpc.Core;
 
 
 using var channel = GrpcChannel.ForAddress("http://localhost:6969");
-    var client = new BrugerService.BrugerServiceClient(channel);
+var client = new BrugerService.BrugerServiceClient(channel);
 
 
-        Bruger helloRequest = new Bruger()
-        {
-            Username = "bigman",
-            Password = "biggerman",
-        };
-        
-        async Task<BrugerResponse> HelloTask()
-    {
-        BrugerResponse helloResponse = await client.createBrugerAsync(helloRequest);
-        Console.WriteLine(helloResponse.Response);
-        return helloResponse;
-        
-    }
-        BrugerResponse response = await HelloTask();
-        Console.WriteLine(response.Response);
-        
+Bruger helloRequest = new Bruger()
+{
+    Username = "bigman5",
+    Password = "biggerman3",
+    DepotID = 400,
+    Saldo = 1337.6969
+};
+
+BrugerRequest request = new BrugerRequest()
+{
+    Username = "bigman3"
+};
+
+async Task<BrugerResponse> HelloTask()
+{
+    BrugerResponse helloResponse = await client.createBrugerAsync(helloRequest);
+    Console.WriteLine(helloResponse.Response);
+    return helloResponse;
+}
+
+async Task<Bruger> getBruger()
+{
+    Bruger bruger = await client.getBrugerAsync(request);
+    Console.WriteLine(bruger.Saldo);
+    return bruger;
+}
+
+//BrugerResponse response = await HelloTask();
+//Console.WriteLine(response.Response);
+Bruger bruger = await getBruger();
+Console.WriteLine(bruger.DepotID);
