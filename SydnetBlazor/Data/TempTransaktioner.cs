7023dd6331b073a69fa3c97830f44eac28a17f84
UshankaBandit <vannicklas@hotmail.com>
2022-12-07 10:29:36 +0100

    UI changes

---
+++ SydnetBlazor/Data/TempTransaktioner.cs
@@ -0,0 +1,10 @@
+﻿namespace SydnetBlazor.Data;
+
+public class TempTransaktioner
+{
+    public string Firma { get; set; }
+    public string Forkortelse { get; set; }
+    public double PriceBought { get; set; }
+    public int Antal { get; set; }
+    public string Dato { get; set; }
+}
